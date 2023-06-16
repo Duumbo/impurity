230fb7e38188502f5db6b2effc37297fef396746
Dimitri Bonanni-Surprenant <bond2102@usherbrooke.ca>
2023-06-16 13:53:34 -0400

    bench: Gutzwiller and Jastrow 8x8

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -4,4 +4,6 @@ mod benchmarks;
 
 criterion_main!(
     benchmarks::pfaffian::benches,
+    benchmarks::jastrow::benches,
+    benchmarks::gutzwiller::benches,
 );
--- benches/benchmarks/mod.rs
+++ benches/benchmarks/mod.rs
@@ -1,1 +1,3 @@
 pub mod pfaffian;
+pub mod jastrow;
+pub mod gutzwiller;
