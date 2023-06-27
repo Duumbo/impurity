0090ff60c7462aa25fdc0c794a0eebe1cb83e2a2
Dimitri Bonanni-Surprenant <bonanni1496dimitri@gmail.com>
2023-06-27 09:04:41 -0400

    Initial pfaffian tested implementation. More tests todo.

--- src/pfaffian.rs
+++ src/pfaffian.rs
@@ -1,5 +1,11 @@
-use crate::{SIZE, FockState};
+use crate::{SIZE, FockState, BitStruct};
 use pfapack::skpfa;
+
+#[derive(Clone, Copy, Debug)]
+enum Spin {
+    Up,
+    Down,
+}
 
 /// Represents the Pfaffian state $\lvert\phi_{\text{PF}}\rangle$.
 /// # Fields
@@ -27,31 +33,33 @@ impl PfaffianState {
 }
 
 pub fn construct_matrix_a_from_state(fij: Vec<f64>, state: FockState) -> PfaffianState {
+    // Fij upup, updown, downup, downdown
     let n = state.spin_up.count_ones() as usize + state.spin_down.count_ones() as usize;
     let mut a: Vec<f64> = vec![0.0; n*n];
 
-    // Get index i (position of the first set bit.)
-    let mut i = state.spin_up.leading_zeros() as usize;
-    let mut spin_up = state.spin_up;
-    let (mut ii, mut jj) = (0, 1);
-    while i < SIZE {
+    let mut indices: Vec<usize> = Vec::with_capacity(n);
+    let mut indices2: Vec<usize> = Vec::with_capacity(n);
+    let (mut spin_up, mut spin_down) = (state.spin_up, state.spin_down);
+    let (mut i, mut j) = (spin_up.leading_zeros(), spin_down.leading_zeros());
+    while (i as usize) < SIZE {
         println!("i: {}", i);
-        let mut spin_down = state.spin_down;
-        let mut j = spin_down.leading_zeros() as usize;
-        while j < SIZE {
-            println!("j: {}", j);
-            a[jj + ii * n] = fij[i + j * SIZE];
-            a[ii + jj * n] = - fij[i + j * SIZE];
-            spin_down ^= 1 << (SIZE - 1 - j);
-            jj += 1;
-            j = spin_down.leading_zeros() as usize;
-        }
-        spin_up ^= 1 << (SIZE - 1 - i);
-        ii += 1;
-        jj = 0;
-        i = spin_up.leading_zeros() as usize;
+        indices.push(i as usize);
+        spin_up ^= 1 << (SIZE - 1 - i as usize);
+        i = spin_up.leading_zeros();
     }
-
+    while (j as usize) < SIZE {
+        println!("j: {}", j);
+        indices2.push(j as usize);
+        spin_down ^= 1 << (SIZE - 1 - j as usize);
+        j = spin_down.leading_zeros();
+    }
+    let off = indices.len();
+    for ii in 0..indices.len() {
+        for jj in 0..indices2.len() {
+            a[ii + (jj + off) * n] = fij[indices[ii] + SIZE*indices2[jj]];
+            a[jj + off + ii * n] = - fij[indices[ii] + SIZE*indices2[jj]];
+        }
+    }
 
     PfaffianState { coeffs: fij, n_elec: n, matrix: a, curr_state: state }
 }
@@ -104,10 +112,26 @@ pub fn compute_pfaffian_wq(a: &mut [f64], n: i32) -> f64 {
 
 #[cfg(test)]
 mod tests {
+    use assert::close;
+    use crate::{pfaffian::compute_pfaffian_wq, SIZE};
+
     use super::construct_matrix_a_from_state;
 
     #[test]
     fn test_pfaffian() {
-        todo!();
+        let mut params = vec![0.0; 4*SIZE*SIZE];
+        // params[i+8*j] = f_ij
+        params[7+8*7] = 1.0;
+        params[7+8*6] = 0.8;
+        params[6+8*7] = 1.0;
+        params[6+8*6] = 0.5;
+        let state = crate::FockState { spin_up: 3, spin_down: 3 };
+        let mut pfstate = construct_matrix_a_from_state(params, state);
+        println!("Matrix: {:?}", pfstate.matrix);
+        close(
+            compute_pfaffian_wq(&mut pfstate.matrix, pfstate.n_elec as i32),
+            0.3,
+            1e-6
+            );
     }
 }
