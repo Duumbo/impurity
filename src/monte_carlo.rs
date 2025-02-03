4943c932232581311a9a344dcae2f1ba47841b45
Dimitri Bonanni-Surprenant <dimitri.bonanni-surprenant@usherbrooke.ca>
2025-02-03 13:46:31 -0500

    (refactor) Refactored the MC loop Also checked at the value for the error estimation.

--- src/monte_carlo.rs
+++ src/monte_carlo.rs
@@ -112,6 +112,106 @@ fn make_update<T: BitOps + std::fmt::Debug + std::fmt::Display + From<u8>>(
     }
 }
 
+#[inline(always)]
+fn warmup<T, R>(
+    rng: &mut R,
+    state: &mut FockState<T>,
+    hop: &mut (usize, usize, Spin),
+    proj: &mut f64,
+    proj_copy_persistent: &mut f64,
+    ratio_prod: &mut f64,
+    pstate: &mut PfaffianState,
+    n_accepted_updates: &mut usize,
+    params: &VarParams,
+    sys: &SysParams
+)
+where T: BitOps + From<u8> + std::fmt::Debug + std::fmt::Display,
+      R: Rng + ?Sized,
+      Standard: Distribution<T>
+{
+    // Warmup
+    for _ in 0..sys.nmcwarmup {
+        let mut proj_copy = *proj;
+        let (mut ratio, state2, col, colidx) = propose_hopping(&state, &pstate, &mut proj_copy, hop, rng, params, sys);
+        trace!("Current state: {}", state);
+        trace!("Proposed state: {}", state2);
+        trace!("Ratio: {}", ratio);
+        ratio *= <f64>::exp(proj_copy - *proj);
+        let w = rng.gen::<f64>();
+        if <f64>::abs(ratio) * <f64>::abs(ratio) >= w {
+            // We ACCEPT
+            trace!("Accept.");
+            *n_accepted_updates += 1;
+            make_update(
+                n_accepted_updates,
+                proj,
+                &mut proj_copy,
+                proj_copy_persistent,
+                &ratio,
+                ratio_prod,
+                state,
+                &state2,
+                &hop,
+                col,
+                colidx,
+                pstate,
+                params,
+                sys
+            );
+
+        }
+    }
+
+}
+
+fn energy_error_estimation(
+    state_energy: f64,
+    previous_energies: &mut [f64],
+    energy_sums: &mut [f64],
+    energy_quad_sums: &mut [f64],
+    n_values: &mut [usize],
+    mc_it: usize,
+    error_estimation_level: usize,
+) {
+    // Energy error estimation
+    let accumulation_level = <f32>::log2((mc_it + 1) as f32) as usize;
+    for i in 0..accumulation_level {
+        if i >= error_estimation_level { break;}
+        if i == accumulation_level - 1{
+            if i == 0 {break;}
+            previous_energies[i] = energy_sums[i - 1];
+        } else {
+            n_values[i] += 1;
+            energy_sums[i] += 0.5 * (previous_energies[i] + state_energy);
+            energy_quad_sums[i] += 0.5 * (previous_energies[i]*previous_energies[i] + state_energy*state_energy);
+        }
+    }
+}
+
+fn accumulate_expvals(energy: &mut f64, state_energy: f64, der: &mut DerivativeOperator) {
+    // Accumulate Energy
+    *energy += state_energy;
+    let last_line_begin = (der.n * der.mu) as usize;
+    let last_line_end = (der.n * (der.mu + 1)) as usize;
+    // Accumulate <HO>
+    unsafe {
+        let incx = 1;
+        let incy = 1;
+        daxpy(
+            der.n,
+            state_energy,
+            &der.o_tilde[last_line_begin .. last_line_end],
+            incx,
+            &mut der.ho,
+            incy
+        );
+    }
+    // Accumulate <O>
+    for i in 0 .. der.n as usize {
+        der.expval_o[i] += der.o_tilde[i + last_line_begin];
+    }
+}
+
 pub fn compute_mean_energy
 <R: Rng + ?Sized,
 T: BitOps + std::fmt::Debug + std::fmt::Display + From<u8>>
@@ -119,6 +219,7 @@ T: BitOps + std::fmt::Debug + std::fmt::Display + From<u8>>
 where Standard: Distribution<T>
 {
     let mut ratiofp = File::create("ratios").unwrap();
+    let mut errorsfp = File::create("errors").unwrap();
     if derivatives.mu != -1 {
         warn!("The derivative operator current row was mu = {} on entry, is it reinitialized?", derivatives.mu);
     }
@@ -133,49 +234,19 @@ where Standard: Distribution<T>
     let mut proj_copy_persistent = proj;
     let mut ratio_prod = 1.0;
     let mut sample_counter: usize = 0;
-    if <f64>::log2(sys.nmcsample as f64) <= 5.0 {
+    if <f64>::log2(sys.nmcsample as f64) <= 6.0 {
         error!("Not enough monte-carlo sample for an accurate error estimation. NMCSAMPLE = {}", sys.nmcsample);
-        panic!("NMCSAMPLE is less than 32.");
+        panic!("NMCSAMPLE is less than 64.");
     }
-    let error_estimation_level = <f64>::log2(sys.nmcsample as f64) as usize - 5;
+    let error_estimation_level = <f64>::log2(sys.nmcsample as f64) as usize - 6;
     let mut energy_sums = vec![0.0; error_estimation_level];
     let mut energy_quad_sums = vec![0.0; error_estimation_level];
     let mut previous_energies = vec![0.0; error_estimation_level + 1];
     let mut n_values = vec![0; error_estimation_level];
 
     info!("Starting the warmup phase.");
-    // Warmup
-    for _ in 0..sys.nmcwarmup {
-        let mut proj_copy = proj;
-        let (mut ratio, state2, col, colidx) = propose_hopping(&state, &pstate, &mut proj_copy, &mut hop, rng, params, sys);
-        trace!("Current state: {}", state);
-        trace!("Proposed state: {}", state2);
-        trace!("Ratio: {}", ratio);
-        ratio *= <f64>::exp(proj_copy - proj);
-        let w = rng.gen::<f64>();
-        if <f64>::abs(ratio) * <f64>::abs(ratio) >= w {
-            // We ACCEPT
-            trace!("Accept.");
-            n_accepted_updates += 1;
-            make_update(
-                &mut n_accepted_updates,
-                &mut proj,
-                &mut proj_copy,
-                &mut proj_copy_persistent,
-                &ratio,
-                &mut ratio_prod,
-                &mut state,
-                &state2,
-                &hop,
-                col,
-                colidx,
-                &mut pstate,
-                params,
-                sys
-            );
-
-        }
-    }
+    warmup(rng, &mut state, &mut hop, &mut proj, &mut proj_copy_persistent, &mut ratio_prod, &mut
+        pstate, &mut n_accepted_updates, params, sys);
 
     info!("Starting the sampling phase.");
     // MC Sampling
@@ -222,42 +293,12 @@ where Standard: Distribution<T>
         }
         if sample_counter >= sys.mcsample_interval {
             accumulated_states.push(state);
-            // Accumulate energy
             derivatives.visited[derivatives.mu as usize] += 1;
             let state_energy = compute_hamiltonian(state, &pstate, proj, params, sys);
-            energy += state_energy;
-            _energy_sq += state_energy*state_energy;
-            // Energy error estimation
-            let accumulation_level = <f32>::log2((mc_it + 1) as f32) as usize;
-            for i in 0..accumulation_level {
-                if i >= error_estimation_level { break;}
-                if i == accumulation_level - 1{
-                    if i == 0 {break;}
-                    previous_energies[i] = energy_sums[i - 1];
-                } else {
-                    n_values[i] += 1;
-                    energy_sums[i] += 0.5 * (previous_energies[i] + state_energy);
-                    energy_quad_sums[i] += 0.5 * (previous_energies[i]*previous_energies[i] + state_energy*state_energy);
-                }
-            }
 
-            // Accumulate <HO_m>
-            unsafe {
-                let incx = 1;
-                let incy = 1;
-                daxpy(
-                    derivatives.n,
-                    state_energy,
-                    &derivatives.o_tilde[(derivatives.n * derivatives.mu) as usize .. (derivatives.n * (derivatives.mu + 1)) as usize],
-                    incx,
-                    &mut derivatives.ho,
-                    incy
-                );
-            }
-            // Accumulate the derivative operators
-            for i in 0 .. derivatives.n as usize {
-                derivatives.expval_o[i] += derivatives.o_tilde[i + (derivatives.n * derivatives.mu) as usize];
-            }
+            accumulate_expvals(&mut energy, state_energy, derivatives);
+            energy_error_estimation(state_energy, &mut previous_energies, &mut energy_sums, &mut
+                energy_quad_sums, &mut n_values, mc_it, error_estimation_level);
             sample_counter = 0;
         }
         sample_counter += 1;
@@ -273,8 +314,8 @@ where Standard: Distribution<T>
         error[i] = <f64>::sqrt(
             (energy_quad_sums[i] - energy_sums[i]*energy_sums[i] / (n_values[i] as f64)) /
             (n_values[i] * (n_values[i] - 1)) as f64
-            )
+            );
     }
     let correlation_time = 0.5 * ((error[error_estimation_level-1]/error[0])*(error[error_estimation_level-1]/error[0]) - 1.0);
-    (energy, accumulated_states, correlation_time)
+    (energy, accumulated_states, error[error_estimation_level - 1])//correlation_time)
 }
